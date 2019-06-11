c7153519d8007b2f3428411786605d2c70b4604e
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2019-06-12 01:22:05 +0200

    feat: lazy-load smoothscroll polyfills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    const supportsSmoothScroll = 'scrollBehavior' in document.documentElement.style
+    if (!supportsSmoothScroll) {
+      Promise.all([
+        import(/* webpackChunkName: "smoothscroll" */ 'smoothscroll-polyfill'),
+        import(/* webpackChunkName: "smoothscroll" */ 'smoothscroll-anchor-polyfill'),
+      ]).then(([smoothscrollPolyfill]) => smoothscrollPolyfill.polyfill())
+    }
+  }
+}
