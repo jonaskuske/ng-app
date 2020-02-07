bbc52d97ada76ce6a520cdcd0841e30797a90a50
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-02-07 20:48:55 +0100

    style: lint fixes

--- src/app/shared/store/selectors.ts
+++ src/app/shared/store/selectors.ts
@@ -3,32 +3,20 @@ import { Entity } from '../models'
 
 export class SharedSelectors {
   static getEntities<T>(stateClass) {
-    return createSelector(
-      [stateClass],
-      state => state.entities as { [id: string]: Entity<T> },
-    )
+    return createSelector([stateClass], state => state.entities as { [id: string]: Entity<T> })
   }
 
   static getEntityList<T>(stateClass) {
-    return createSelector(
-      [stateClass],
-      state => Object.values(state.entities) as Entity<T>[],
-    )
+    return createSelector([stateClass], state => Object.values(state.entities) as Entity<T>[])
   }
 
   static getEntityIds(stateClass) {
-    return createSelector(
-      [stateClass],
-      state => Object.keys(state.entities),
-    )
+    return createSelector([stateClass], state => Object.keys(state.entities))
   }
 
   static getEntityListByPageFn(stateClass) {
     return (pageIds: number[]) => {
-      return createSelector(
-        [stateClass],
-        state => pageIds.map(id => state.entities[id]),
-      )
+      return createSelector([stateClass], state => pageIds.map(id => state.entities[id]))
     }
   }
 }
